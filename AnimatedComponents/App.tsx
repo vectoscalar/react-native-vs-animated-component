ae97b1ac8bc44835c7d4a9cf24384934c4783a15
Pranjul Parnami <pranjul.parnami@vectoscalar.com>
2024-04-12 21:31:42 +0530

    Marquee Text

--- AnimatedComponents/App.tsx
+++ AnimatedComponents/App.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo, useState } from 'react'
-import { SafeAreaView } from 'react-native'
+import { Easing, SafeAreaView, View } from 'react-native'
 
 import { RemixIcons } from '@constants'
 
@@ -7,6 +7,7 @@ import ProgressButton from './src/components/buttons/progress-button/ProgressBut
 import SpringButton from './src/components/buttons/spring-button/SpringButton'
 import SwipeButton from './src/components/buttons/swipe-button/SwipeButton'
 import Select from './src/components/select/Select'
+import SBTextAnimator from './src/components/text-animator/TextAnimator'
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(false)
@@ -63,6 +64,19 @@ const App = () => {
         taskStatusData={taskStatusData}
         thumbColors={thumbColors}
       />
+      <SBTextAnimator
+        bounce={false}
+        duration={3000}
+        easing={Easing.linear}
+        isRTL={false}
+        loop
+        marqueeDelay={0}
+        repeatSpacer={30}
+        scroll={false}
+        shouldAnimateThreshold={40}>
+        Textas dasjkdjkas jckdj kasdjkajkd kjsakldkasld Textas dasjkdjkas jckdj kasdjkajkd
+        kjsakldkasld Textas dasjkdjkas jckdj kasdjkajkd kjsakldkasld Textas dasjkdjkas jckdj
+      </SBTextAnimator>
     </SafeAreaView>
   )
 }
