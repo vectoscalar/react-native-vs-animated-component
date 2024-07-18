e100c7c314c6ff2ecc6c5afe757d01bfe29981c4
Aman <aman@MacBook-Pro.local>
2024-07-18 16:46:02 +0530

    npm-testing: test select, spring button and progress button

--- AnimatedComponents/App.tsx
+++ AnimatedComponents/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { SafeAreaView, Text } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
+import { ProgressButton, Select, SpringButton } from 'animated-tests-8-npm-public'
+
 const App = () => {
+  const [selectedOption, setSelectedOption] = useState({ title: '', value: '' })
+  const [isloading, setIsloading] = useState(false)
+  console.log(selectedOption)
   return (
     <SafeAreaView>
       <Text>App</Text>
       <Icon name="arrow-back" size={40} />
+      <Select
+        onChange={setSelectedOption}
+        options={[{ title: 'aman', value: 'hello' }]}
+        selectedOption={selectedOption}
+      />
+      <SpringButton label="test" onPress={() => {}} />
+      <ProgressButton
+        label="loading"
+        onPress={() => {
+          setIsloading(true)
+        }}
+        isLoading={isloading}
+      />
     </SafeAreaView>
   )
 }
--- AnimatedComponents/declarations.d.ts
+++ AnimatedComponents/declarations.d.ts
@@ -5,3 +5,5 @@ declare module '*.svg' {
   const content: React.FC<SvgProps>
   export default content
 }
+
+declare module 'animated-tests-8-npm-public'
